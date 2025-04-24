8cb376ee10aa7dcce5d0b53c4abd4098a7f06aca
irrenaivanova <150434502+irrenaivanova@users.noreply.github.com>
2025-04-24 11:16:26 +0300

    Create initial React project and add favicon

---
+++ src/client/src/Components/Logo.tsx
@@ -0,0 +1,11 @@
+import { GiFairyWings } from "react-icons/gi";
+
+const Logo = () => {
+  return (
+    <div>
+      <GiFairyWings color="#2138f75" size={100}/>
+    </div>
+  )
+}
+
+export default Logo
